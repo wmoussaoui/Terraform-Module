212c54f90d084d236a61ba1b0979744ec0a0d4da
wmoussaoui <walid.moussaoui@neurones.net>
2020-01-21 15:48:27 +0100

    aks added

---
+++ module-aks/main.tf
@@ -0,0 +1,32 @@
+#---- create resource kubernetes cluster -----------
+
+resource "azurerm_kubernetes_cluster" "aks" {
+    name                = var.aks_cluster_name
+    kubernetes_version = var.kubernetes_version
+    location            = var.location_var
+    resource_group_name = var.rg_name
+    dns_prefix          = var.aks_dns_prefix
+    kubernetes_version  = var.aks_k8s_version
+    role_based_access_control {
+      enabled = "true"
+    }
+    default_node_pool {
+        name            = var.name_node_pool
+        vm_size         = var.aks_vm_size
+        os_disk_size_gb = var.aks_os_disk_size_gb
+        vnet_subnet_id	= var.vnet_subnet_id
+        enable_auto_scaling = var.enable_auto_scaling
+        type            = var.type_auto_scaling
+        min_count       = var.min_count 
+        max_count       = var.max_count 
+        
+    }
+    service_principal {
+          client_id = var.client_id 
+          client_secret = var.client_secret
+
+    }
+    tags = "${var.tags}"    
+   
+      }
+      
---
+++ module-aks/variables.tf
@@ -0,0 +1,80 @@
+variable "vnet_subnet_id" {} 
+ 
+
+
+variable "tags" {
+  description = "The tags to associate with your network and subnets."
+  type        = map(string)
+
+  default = {
+    tag1 = ""
+    tag2 = ""
+  }
+}
+#----------- variables cluster AKS --------------------------
+variable "aks_cluster_name"{
+  type = string
+}
+variable "aks_dns_prefix" {
+  type = string
+  default = "akscluster-swf-fff"
+}
+variable "aks_k8s_version" {
+  type = string
+  default = "1.14.8"
+}
+variable aks_vm_size {
+  description = "The size of vm"
+  type = string
+  default = "Standard_B4ms"
+}
+variable aks_os_disk_size_gb {
+  description = "The size of disk"
+  default = 32
+}
+variable client_id {}
+
+variable client_secret {}
+
+
+#------------------------ressource-groupe-------------------------------------
+
+  variable "rg_name" {
+  description = "Name of the Resource Group for the hub virtual network"
+  type = string
+  }
+#------------------------------location-------------------------------
+
+  variable "location_var" {
+  type = string
+  default = "West Europe"
+}
+variable "enable_auto_scaling" {
+  default = "true"
+}
+variable "type_auto_scaling" {
+  default = "VirtualMachineScaleSets"
+}
+variable "min_count" {
+  default = "1"
+}
+variable "max_count" {
+  default = "3"
+}
+
+variable "kubernetes_version" {
+    default = "1.14.8"
+}
+
+variable "name_node_pool" {
+    default = "aks"
+}
+
+
+
+
+
+
+
+
+
