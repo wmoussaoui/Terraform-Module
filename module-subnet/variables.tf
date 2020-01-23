ed6d418701eff6e017e00954b11cbf9609faee62
wmoussaoui <60008632+wmoussaoui@users.noreply.github.com>
2020-01-23 19:06:40 +0100

    Update variables.tf

--- module-subnet/variables.tf
+++ module-subnet/variables.tf
@@ -23,4 +23,9 @@ variable "tags" {
         "projet" = "fff",
         "confidentialite" = "fff"}]
 }
+variable "custom_rules" {
+  description = "Security rules for the network security group using this format name = [priority, direction, access, protocol, source_port_range, destination_port_range, source_address_prefix, destination_address_prefix, description]"
+  type        = list(any)
+  default     = []
+}
   
