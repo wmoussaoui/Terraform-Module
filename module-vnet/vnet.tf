ccc4f53bf1b328e79b47704dae785bb35ab91963
wmoussaoui <60008632+wmoussaoui@users.noreply.github.com>
2020-01-23 17:12:01 +0100

    Update vnet.tf

--- module-vnet/vnet.tf
+++ module-vnet/vnet.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "virtual_network" {
-  address_space       = [element(var.address_space, count.index)] #"${var.address_space[count.index]}"
+  address_space       = [element(var.address_space, count.index)] 
   location            = "${var.location}"
   name                = var.name[count.index]
   resource_group_name = element(var.resource_group_name, count.index)
