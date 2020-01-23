860ef309635282851b6a366482ed1ac12415095d
wmoussaoui <60008632+wmoussaoui@users.noreply.github.com>
2020-01-23 18:44:39 +0100

    Update variables.tf

--- module-subnet/variables.tf
+++ module-subnet/variables.tf
@@ -8,4 +8,19 @@ variable "name" {
 }
 variable "resource_group_name" {}
 variable "virtual_network_name" {}  
+
+#------------------------------ tags sg------------------------------
+variable "tags" {
+  description = "The tags to associate with your network and subnets."
+  #type        = map(string)
+
+  default = [{
+        "code_appli" = "fff", 
+        "composant_role" = "fff", 
+        "environnement" = "fff",
+        "Version" = "fff", 
+        "proprietaire" = "fff",
+        "projet" = "fff",
+        "confidentialite" = "fff"}]
+}
   
