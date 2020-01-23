5a140f88afff582336b0b20ed9c2064362e943be
wmoussaoui <60008632+wmoussaoui@users.noreply.github.com>
2020-01-23 18:17:08 +0100

    Update main.tf

--- module-subnet/main.tf
+++ module-subnet/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_subnet" "default" {
     name                 = var.name[count.index]
-    address_prefix       = [element(var.address_prefix, count.index)] 
+    address_prefix       = element(var.address_prefix, count.index) 
     resource_group_name  = element(var.resource_group_name, count.index)
     virtual_network_name = var.virtual_network_name
     count                = length(var.name)
