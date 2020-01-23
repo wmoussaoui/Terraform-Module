8b15e03b72c9ddcdd0b38708ab2aba5bbaeb4582
wmoussaoui <60008632+wmoussaoui@users.noreply.github.com>
2020-01-23 18:44:01 +0100

    Update main.tf

--- module-subnet/main.tf
+++ module-subnet/main.tf
@@ -5,3 +5,36 @@ resource "azurerm_subnet" "default" {
     virtual_network_name = var.virtual_network_name
     count                = length(var.name)
 }
+
+
+
+#--------- create resource security group ---------
+resource "azurerm_network_security_group" "sg" {
+  name                = var.sg_name[count.index]
+  location            = var.location_var
+  resource_group_name = var.rg_name
+  count                = length(var.sg_name)
+  security_rule {
+    name                       = "rule-swf-fff"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+    tags = {
+        ApplicationID = var.applicationid_tags
+        Environment = var.environnement_tags
+        Version = var.version_tags
+        Owner = var.owner_tags
+        Department = var.departement_tags
+        CostCenter = var.costcenter_tags
+        Customer = var.customer_tags
+        Project = var.projet_tags
+  }
+}   
+
+
