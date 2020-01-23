157871309f163d26193081417efb2ee5c50e91d9
wmoussaoui <60008632+wmoussaoui@users.noreply.github.com>
2020-01-23 17:59:09 +0100

    Update variables.tf

--- module-subnet/variables.tf
+++ module-subnet/variables.tf
@@ -4,10 +4,6 @@ variable "address_prefix" {
 variable "name" {
  
 }
-variable "resource_group.name" {
-  
-}
-variable "virtual_network.name" {
- 
-}  
+variable "resource_group.name" {}
+variable "virtual_network.name" {}  
   
