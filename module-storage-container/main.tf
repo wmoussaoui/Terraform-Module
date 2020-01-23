1749a51d242d26f0f967477fb97210e11e0e018e
wmoussaoui <60008632+wmoussaoui@users.noreply.github.com>
2020-01-23 11:24:32 +0100

    Update main.tf

--- module-storage-container/main.tf
+++ module-storage-container/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_container" "storage-container-hub" {
   name                  = var.name[count.index]
-  storage_account_name  = var.storage_account_name
+  storage_account_name  =  "${element(var.storage_account_name, count.index)}"
   container_access_type = var.container_access_type
   count    = length(var.name)
   
