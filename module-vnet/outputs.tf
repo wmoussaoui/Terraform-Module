5422924e2531193f1fc3d0751cddda2b3d19097a
wmoussaoui <60008632+wmoussaoui@users.noreply.github.com>
2020-01-23 16:31:55 +0100

    Update outputs.tf

--- module-vnet/outputs.tf
+++ module-vnet/outputs.tf
@@ -8,9 +8,6 @@ output "address_space" {
   value = "${azurerm_virtual_network.virtual_network.*.address_space}"
 }
 
-output "location" {
-  value = "${azurerm_virtual_network.virtual_network.location}"
-}
 
 output "vnet_id" {
   value = "${azurerm_virtual_network.virtual_network.*.id}"
