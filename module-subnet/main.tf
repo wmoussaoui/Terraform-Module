75ce8bce1bfa2c45c426c5be64bc35a47e209cfe
wmoussaoui <60008632+wmoussaoui@users.noreply.github.com>
2020-01-24 13:14:28 +0100

    Update main.tf

--- module-subnet/main.tf
+++ module-subnet/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_network_security_group" "sg" {
   name                = var.sg_name[count.index]
   location            = var.location_var
   resource_group_name = element(var.sg_resource_group_name, count.index)
-  count                = length(var.sg_name)  
+  count                = "${length(var.sg_name) : ""}"  
   tags                     = element(var.tags, count.index)
     
 }   
