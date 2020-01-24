8fa928b36634b38530004973f00325ecfe5efa8c
wmoussaoui <60008632+wmoussaoui@users.noreply.github.com>
2020-01-24 15:06:29 +0100

    Update variables.tf

--- module-subnet/variables.tf
+++ module-subnet/variables.tf
@@ -1,14 +1,14 @@
 
 
-variable "nsg_ids" {
-  description = "A map of subnet name to Network Security Group IDs"
+#variable "nsg_ids" {
+ # description = "A map of subnet name to Network Security Group IDs"
   #type        = map(string)
 
-  default = [{
-    subnet1 = "nsgid1"
-    subnet3 = "nsgid3"
-  }]
-}
+  #default = [{
+   # subnet1 = "nsgid1"
+    #subnet3 = "nsgid3"
+  #}]
+#}
 
 
 variable "address_prefix" {
@@ -24,38 +24,38 @@ variable "resource_group_name" {}
 variable "virtual_network_name" {}  
 
 #------------------------------ tags sg------------------------------
-variable "tags" {
-  description = "The tags to associate with your network and subnets."
+#variable "tags" {
+  #description = "The tags to associate with your network and subnets."
   #type        = map(string)
 
-  default = [{
-        "code_appli" = "fff", 
-        "composant_role" = "fff", 
-        "environnement" = "fff",
-        "Version" = "fff", 
-        "proprietaire" = "fff",
-        "projet" = "fff",
-        "confidentialite" = "fff"}]
-}
-variable "custom_rules" {
-  description = "Security rules for the network security group using this format name = [priority, direction, access, protocol, source_port_range, destination_port_range, source_address_prefix, destination_address_prefix, description]"
-  type        = list(any)
-  default     = []
-}
-variable "sg_name" {
-  type = any
+  #default = [{
+       # "code_appli" = "fff", 
+      #  "composant_role" = "fff", 
+     #   "environnement" = "fff",
+    #    "Version" = "fff", 
+   #     "proprietaire" = "fff",
+  #      "projet" = "fff",
+ #       "confidentialite" = "fff"}]
+#}
+#variable "custom_rules" {
+  #description = "Security rules for the network security group using this format name = [priority, direction, access, protocol, source_port_range, destination_port_range, source_address_prefix, destination_address_prefix, description]"
+  #type        = list(any)
+ # default     = []
+#}
+#variable "sg_name" {
+ # type = any
 
-}
+#}
 variable "location_var" {
  default = "West Europe"
 }
 
-variable "sg_resource_group_name" {
- default = []
-}
-variable "rule_resource_group_name" {
- default = []
-}
+#variable "sg_resource_group_name" {
+# default = []
+#}
+#variable "rule_resource_group_name" {
+# default = []
+#}
 
 
 
