a5d6f51f6795f5cd4286177bb84e7ab3b3c0360e
wmoussaoui <60008632+wmoussaoui@users.noreply.github.com>
2020-01-24 12:29:16 +0100

    Update main.tf

--- module-subnet/main.tf
+++ module-subnet/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_subnet" "default" {
 
 #--------- create resource security group ---------
 resource "azurerm_network_security_group" "sg" {
-  name                = var.sg_name[count.index]
+  name                = element(var.sg_name, count.index)
   location            = var.location_var
   resource_group_name = element(var.sg_resource_group_name, count.index)
   count                = length(var.name)  
