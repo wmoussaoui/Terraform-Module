9745ae8d6ce5a32b82b895d4070bac3339d4b5da
wmoussaoui <60008632+wmoussaoui@users.noreply.github.com>
2020-01-24 14:59:08 +0100

    Update main.tf

--- module-subnet/main.tf
+++ module-subnet/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_subnet" "default" {
     resource_group_name  = element(var.resource_group_name, count.index)
     virtual_network_name = var.virtual_network_name
     count                = length(var.name)
-    network_security_group_id = "${element(azurerm_network_security_group.sg.*.id, count.index)}"
+    #network_security_group_id = "${element(azurerm_network_security_group.sg.*.id, count.index)}"
     
 }
 
@@ -20,30 +20,30 @@ resource "azurerm_network_security_group" "sg" {
     
 }   
 
-#resource "azurerm_network_security_rule" "custom_rules" {
-#  count                       = length(var.custom_rules)
-#  name                        = lookup(var.custom_rules[count.index], "name", "default_rule_name")
-#  priority                    = lookup(var.custom_rules[count.index], "priority")
-#  direction                   = lookup(var.custom_rules[count.index], "direction", "Any")
-#  access                      = lookup(var.custom_rules[count.index], "access", "Allow")
-#  protocol                    = lookup(var.custom_rules[count.index], "protocol", "*")
-#  source_port_ranges          = split(",", replace(lookup(var.custom_rules[count.index], "source_port_range", "*"), "*", "0-65535"))
-#  destination_port_ranges     = split(",", replace(lookup(var.custom_rules[count.index], "destination_port_range", "*"), "*", "0-65535"))
-#  source_address_prefix       = lookup(var.custom_rules[count.index], "source_address_prefix", "*")
-#  destination_address_prefix  = lookup(var.custom_rules[count.index], "destination_address_prefix", "*")
-#  description                 = lookup(var.custom_rules[count.index], "description", "Security rule for ${lookup(var.custom_rules[count.index], "name", "default_rule_name")}")
-#  resource_group_name         = element(var.rule_resource_group_name, count.index)
-#  network_security_group_name =  "${azurerm_network_security_group.sg.*.name}"
+resource "azurerm_network_security_rule" "custom_rules" {
+  count                       = length(var.custom_rules)
+  name                        = lookup(var.custom_rules[count.index], "name", "default_rule_name")
+  priority                    = lookup(var.custom_rules[count.index], "priority")
+  direction                   = lookup(var.custom_rules[count.index], "direction", "Any")
+  access                      = lookup(var.custom_rules[count.index], "access", "Allow")
+  protocol                    = lookup(var.custom_rules[count.index], "protocol", "*")
+  source_port_ranges          = split(",", replace(lookup(var.custom_rules[count.index], "source_port_range", "*"), "*", "0-65535"))
+  destination_port_ranges     = split(",", replace(lookup(var.custom_rules[count.index], "destination_port_range", "*"), "*", "0-65535"))
+  source_address_prefix       = lookup(var.custom_rules[count.index], "source_address_prefix", "*")
+  destination_address_prefix  = lookup(var.custom_rules[count.index], "destination_address_prefix", "*")
+  description                 = lookup(var.custom_rules[count.index], "description", "Security rule for ${lookup(var.custom_rules[count.index], "name", "default_rule_name")}")
+  resource_group_name         = element(var.rule_resource_group_name, count.index)
+  network_security_group_name =  "${azurerm_network_security_group.sg.*.name}"
 }
 
 #---- create resource security group association ------
 
-#resource "azurerm_subnet_network_security_group_association" "ass" {
+resource "azurerm_subnet_network_security_group_association" "ass" {
     
- # count                     = length(var.name)  
- # subnet_id                 = "${element(azurerm_subnet.default.*.id, count.index)}" 
- # network_security_group_id = "${element(azurerm_network_security_group.sg.*.id, count.index)}" 
-#}
+  count                     = length(var.name)  
+  subnet_id                 = "${element(azurerm_subnet.default.*.id, count.index)}" 
+  network_security_group_id = "${element(azurerm_network_security_group.sg.*.id, count.index)}" 
+}
 
 
 
