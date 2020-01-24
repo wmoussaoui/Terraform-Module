f22637a382b8818baae12479415cc8fe911063ed
wmoussaoui <60008632+wmoussaoui@users.noreply.github.com>
2020-01-24 16:20:12 +0100

    Update variables.tf

--- module-subnet/variables.tf
+++ module-subnet/variables.tf
@@ -24,19 +24,19 @@ variable "resource_group_name" {}
 variable "virtual_network_name" {}  
 
 #------------------------------ tags sg------------------------------
-#variable "tags" {
-  #description = "The tags to associate with your network and subnets."
-  #type        = map(string)
+variable "tags" {
+  description = "The tags to associate with your network and subnets."
+  type        = map(string)
 
-  #default = [{
-       # "code_appli" = "fff", 
-      #  "composant_role" = "fff", 
-     #   "environnement" = "fff",
-    #    "Version" = "fff", 
-   #     "proprietaire" = "fff",
-  #      "projet" = "fff",
- #       "confidentialite" = "fff"}]
-#}
+  default = [{
+        "code_appli" = "fff", 
+        "composant_role" = "fff", 
+        "environnement" = "fff",
+        "Version" = "fff", 
+        "proprietaire" = "fff",
+        "projet" = "fff",
+        "confidentialite" = "fff"}]
+}
 #variable "custom_rules" {
   #description = "Security rules for the network security group using this format name = [priority, direction, access, protocol, source_port_range, destination_port_range, source_address_prefix, destination_address_prefix, description]"
   #type        = list(any)
@@ -50,9 +50,9 @@ variable "location_var" {
  default = "West Europe"
 }
 
-#variable "sg_resource_group_name" {
-# default = []
-#}
+variable "sg_resource_group_name" {
+ default = []
+}
 #variable "rule_resource_group_name" {
 # default = []
 #}
